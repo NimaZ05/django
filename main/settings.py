210d181b4213fb5da681934687d9edecae9df65d
NimaZ05 <nimaz2138487@gmail.com>
2025-08-11 13:15:15 +0330

    added static directories and urls in project
    static root, static urls, media url, media root

--- main/settings.py
+++ main/settings.py
@@ -117,12 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-if DEBUG:
-    STATIC_URL = 'static/'
-    STATIC_ROOT = BASE_DIR / 'static'
-else:
-    MEDIA_URL = 'media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
