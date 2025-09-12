2130b123710cd942020ceeea0e6a831eea0922d1
NimaZ05 <nimaz2138487@gmail.com>
2025-09-12 19:15:07 +0330

    creating templatetags and using them in templates
    adding favicon2
    creating inclusion templates and templatetags

--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
-def blog_view(request):
+def blog_view(request, cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {
         'posts': posts
-        }
+    }
     return render(request, 'blog/blog-home.html', context)
 
 
 def blog_single(request, pid):
-    #  posts = Post.objects.filter(status=1)
     post = get_object_or_404(Post, pk=pid, status=1)
     session_key = f'viewed_post_{post.pk}'
     if not request.session.get(session_key, False):
         post.counted_view += 1
         post.save(update_fields=['counted_view'])
         request.session[session_key] = True
-        
+
     next_post = Post.objects.filter(pk__gt=post.pk).order_by('pk').first()
     prev_post = Post.objects.filter(pk__lt=post.pk).order_by('-pk').first()
-    
-    
+
     context = {
         'post': post,
         'next_post': next_post,
@@ -31,7 +31,14 @@ def blog_single(request, pid):
     return render(request, 'blog/blog-single.html', context)
 
 
-def test(request, pid):
-    post = get_object_or_404(Post, pk=pid)
-    context = {'post': post}
-    return render(request, 'test.html', context)
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog-home.html', context)
+
+
+def test(request):
+    return render(request, 'test.html')
